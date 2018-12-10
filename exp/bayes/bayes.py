a88a2d9b4bf5bf35169b4514639301638558d6a0
wsg <wu5040@gmail.com>
2018-12-10 12:07:52 +0800

    完善一点点...

--- exp/bayes/bayes.py
+++ exp/bayes/bayes.py
@@ -1,23 +1,27 @@
 import numpy as np
 from math import sqrt
 import matplotlib.pyplot as plt
-#创建实验样本集
-def loadDataSet():
+
+#创建实验样本集,intX--属性值
+def loadDataSet(intX):
     file_data1 = np.loadtxt(
         r'/home/wsg/111aaa/PR_Course/expData/genderdata/boy.txt', delimiter='\t')
     file_data2 = np.loadtxt(
         r'/home/wsg/111aaa/PR_Course/expData/genderdata/girl.txt', delimiter='\t')
 
-    groupList = []
-    classVec = []
+    groupList1 = []
+    classVec1 = []
     for dataItem in file_data1:
-        groupList.append(dataItem[0])
-        classVec.append(1)
+        groupList1.append(dataItem[intX])
+        classVec1.append(1)
+    
+    groupList2 = []
+    classVec2 = []
     for dataItem in file_data2:
-        groupList.append(dataItem[0])
-        classVec.append(0)
+        groupList2.append(dataItem[intX])
+        classVec2.append(0)
 
-    return groupList,classVec
+    return groupList1,classVec1,groupList2,classVec2
 
 
 def createFeatureList(dataSet):
@@ -35,55 +39,65 @@ def bagOfFeature2Vec(featureList,inputSet):
             print("the feature: %s is not in my FeatureList!" % item)
     return returnVec
 
-
-
-listOPosts,listClasses=loadDataSet()
-
-print(listOPosts,listClasses)
-
-u_sum=0
-u_len=0
-for i in range(len(listOPosts)):
-    if(listClasses[i]==1):
-        u_sum+=listOPosts[i]
-        u_len+=1
-
-u=u_sum/u_len
-print(u)
-
-o2_sum=0
-for i in range(len(listOPosts)):
-    if(listClasses[i]==1):
-        o2_sum+=(listOPosts[i]-u)**2
-
-o2=o2_sum/u_len
-print(o2)
-
 #normfun正态分布函数，mu: 均值，sigma:标准差，pdf:概率密度函数，np.exp():概率密度函数公式
-def normfun(x, mu, sigma):
+def normFun(x, mu, sigma):
     pdf = np.exp(-((x - mu)**2) / (2* sigma**2)) / (sigma * np.sqrt(2*np.pi))
     return pdf
 
-# x的范围为60-150，以1为单位,需x根据范围调试
-x = np.arange(140, 200, 1)
+def GaussianFun(ListX,listClasses):
+    mu_sum=0
+    mu_len=0
+    for i in range(len(ListX)):
+            mu_sum+=ListX[i]
+            mu_len+=1
 
-# x数对应的概率密度
-y = normfun(x, u, sqrt(o2))
+    mu=mu_sum/mu_len
+    print("mu=",mu)
 
-# 参数,颜色，线宽
-plt.plot(x,y, color='g',linewidth = 3)
+    sigma2_sum=0
+    for i in range(len(ListX)):
+            sigma2_sum+=(ListX[i]-mu)**2
 
-plt.title('IQ distribution')
-plt.xlabel('Feature')
-plt.ylabel('Probability')
+    sigma2=sigma2_sum/mu_len
+    print("sigma2=",sigma2)
+
+    return mu,sigma2
+
+def funImg(pn,x,mu,sigma2):
+
+    y1 = normFun(x, mu[0], sqrt(sigma2[0]))
+    y2 = normFun(x, mu[1], sqrt(sigma2[1]))
+
+
+    # 参数,颜色，线宽
+    pn.plot(x,y1, color='b',linewidth = 3,label="male")
+    pn.plot(x,y2, color='r',linewidth = 3,label="female")
+
+    pn.legend()
+    # pn.title('GaussianFun')
+    pn.set_xlabel('Feature')
+    pn.set_ylabel('Probability')
+
+plt.figure(figsize=(8,8))
+
+x=[]
+x.append(np.arange(130,200,1))
+x.append(np.arange(30,100,1))
+x.append(np.arange(30,50,1))
+pn=[]
+pn.append(plt.subplot(311))
+pn.append(plt.subplot(312))
+pn.append(plt.subplot(313))
+
+
+for i in range(3):
+    mListX,mListClasses,fListX,fListClasses=loadDataSet(i)
+    mu=[0,0]
+    sigma2=[0,0]
+
+    mu[0],sigma2[0]=GaussianFun(mListX,mListClasses)
+    mu[1],sigma2[1]=GaussianFun(fListX,fListClasses)
+
+    funImg(pn[i],x[i],mu,sigma2)
+
 plt.show()
-
-
-
-
-
-
-
-
-
-
