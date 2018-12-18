a482376f66c7c374ff627a1286579f8ef9f71a32
wsg <wu5040@gmail.com>
2018-12-19 00:53:58 +0800

    LOO

---
+++ exp/bayes/eg.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+#分别存放所有点的横坐标和纵坐标，一一对应
+# data = [1,2,3,3.5,6,5]
+# y_list = [1,3,4,5,4,7]
+# data = data[data[:,2].argsort()]
+# #创建图并命名
+# plt.figure('Line fig')
+# ax = plt.gca()
+# #设置x轴、y轴名称
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+
+# #画连线图，以x_list中的值为横坐标，以y_list中的值为纵坐标
+# #参数c指定连线的颜色，linewidth指定连线宽度，alpha指定连线的透明度
+# ax.plot(x_list, y_list, color='r', linewidth=1, alpha=0.6)
+
+# plt.show()
+list1 = [[1, 3, 2], [3, 5, 4],[2,3,4]]
+array = np.array(list1)
+array=array[array[:,0].argsort()]
+print(array)
